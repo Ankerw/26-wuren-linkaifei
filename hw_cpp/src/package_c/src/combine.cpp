00c4ca176b9e4ff34355886e45da078cf2d6f1b3
Anker <you@example.com>
2025-07-25 15:05:00 +0800

    hw_cpp,correction

--- hw_cpp/src/package_c/src/combine.cpp
+++ hw_cpp/src/package_c/src/combine.cpp
@@ -14,32 +14,33 @@
 int main(int argc,char* argv[])
 {
     ros::init(argc,argv,"combine");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh;//初始化节点
 
     ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("/lidar_points", 10);
-
+    //创建发布器，设置topic
 
     rosbag::Bag bag;
     bag.open("../bag/carla_test.bag",rosbag::bagmode::Read);
 
     rosbag::View view(bag,rosbag::TopicQuery({"/carla/ego_vehicle/lidar_up","/carla/ego_vehicle/lidar_mid","/carla/ego_vehicle/lidar_down"}));
+    //读取三个话题
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZI>);//先声明，pcl版的目标点云
 
 //act
     for(const rosbag::MessageInstance& msg_inst : view)
     {
         sensor_msgs::PointCloud2::ConstPtr ptr
-        = msg_inst.instantiate<sensor_msgs::PointCloud2>();
+        = msg_inst.instantiate<sensor_msgs::PointCloud2>();//ptr指向点云消息
 
-        pcl::PointCloud<pcl::PointXYZI>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::fromROSMsg(*ptr, *temp_cloud);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZI>);//声明中间量
+        pcl::fromROSMsg(*ptr, *temp_cloud);//转成pcl下
 
         
         for(auto& i:temp_cloud->points)
         {
-            i.intensity = i.intensity * 256;
-            target_cloud->points.push_back(i);
+            i.intensity = i.intensity * 256; //看了一下强度，大多在0.9到1之间，我都乘了256
+            target_cloud->points.push_back(i);//依次往target添加点
         }
 
     
@@ -50,18 +51,19 @@ int main(int argc,char* argv[])
 
 
 
-    bag.close();
+    bag.close();//关闭
 
 
 
 
     sensor_msgs::PointCloud2 final_msg;
-    pcl::toROSMsg(*target_cloud, final_msg);
-    final_msg.header.frame_id = "lidar";
-    final_msg.header.stamp = ros::Time::now();
+    pcl::toROSMsg(*target_cloud, final_msg); //转回来，转成 sensor_msgs::PointCloud2
+    final_msg.header.frame_id = "lidar";  //唯一指定frame_id
+    final_msg.header.stamp = ros::Time::now();//时间戳
   
 
     ros::Rate pub_rate(0.05); 
+    
   
 
 
@@ -70,7 +72,7 @@ int main(int argc,char* argv[])
     pub.publish(final_msg);
     pub_rate.sleep();
     ros::spinOnce();
-    }
+    } //设置频率，发布final_msg
   
 
 }
--- hw_cpp/src/package_c/src/tran.cpp
+++ hw_cpp/src/package_c/src/tran.cpp
@@ -17,24 +17,23 @@
 
 
 Eigen::Matrix<double,3,4> waican;
-
-
 Eigen::Matrix<double,3,3> neican;
+//外参和内参矩阵，最开始忘了要在函数内赋值，只剩下了声明，赋值放到了main里
 
 
 
 
 
-Eigen::Matrix<double,2,1> transfer(const geometry_msgs::Point& pos)
-{
+Eigen::Matrix<double,2,1> transfer(const geometry_msgs::Point& pos) //用来接受单个锥桶消息里的Point，输出2×1的vector
+{   
     Eigen::Matrix<double,4,1> mat;
-    mat << pos.x,pos.y,pos.z,1;
+    mat << pos.x,pos.y,pos.z,1;     //要用4×1的列向量表示原来坐标，补一个1
 
-    Eigen::Matrix<double,3,1> cone_will_in_pixel = neican * waican * mat;
+    Eigen::Matrix<double,3,1> cone_will_in_pixel = neican * waican * mat;  //依次乘外参和内参
     Eigen::Matrix<double,2,1> cone_in_pixel;
 
     cone_in_pixel(0,0) = cone_will_in_pixel(0,0) /cone_will_in_pixel(2,0);
-    cone_in_pixel(1,0) = cone_will_in_pixel(1,0) /cone_will_in_pixel(2,0);
+    cone_in_pixel(1,0) = cone_will_in_pixel(1,0) /cone_will_in_pixel(2,0); //把（x，y，z）里的z化为1，投到像素平面上
 
     return cone_in_pixel;
 }
@@ -54,27 +53,27 @@ int main(int argc,char* argv[])
   neican<<532.795, 0.0, 632.15,
   0.0, 532.72, -3.428,
   0.0, 0.0, 1.0;
-
+//赋值，两个参数矩阵
 
     ros::init(argc,argv,"tran");
     ros::NodeHandle nh;
+//初始化node
 
     image_transport::ImageTransport it(nh);
     image_transport::Publisher pub = it.advertise("/projected_image", 10);
-
+//要发布可视化结果，用image_transport库创建图像发布器
 
     rosbag::Bag bag;
     bag.open("../bag/cone_detected.bag",rosbag::bagmode::Read);
-
     rosbag::View view(bag,rosbag::TopicQuery("/perception/fusion/cone_fusion"));
     //rosbag::View view(bag,rosbag::TopicQuery("/perception/lidar/cone_detections"));
-
+//读取包里的消息，ppt里的好像有误，用rosbag info得到另一个消息类型
 
     cv::Mat img(360,1280,CV_8UC3,cv::Scalar(255,255,255));
     std::vector<cv::Point2f> points;
+//背景，放像素点的可变数组
 
-
-    for(const rosbag::MessageInstance& msg_inst : view)
+    for(const rosbag::MessageInstance& msg_inst : view)  //新学到的用法for（单个成员：容器），
     {
         fsd_common_msgs::ConeDetections::ConstPtr cone_array_ptr
         = msg_inst.instantiate<fsd_common_msgs::ConeDetections>();
@@ -83,9 +82,9 @@ int main(int argc,char* argv[])
         {
 
 
-            for(const auto& cone : cone_array_ptr->cone_detections){
+            for(const auto& cone : cone_array_ptr->cone_detections){  //cone_array_ptr访问ConeDetections里的cone_detections，这是一个cone数组
                 {
-                    Eigen::Matrix<double,2,1> cone_in_pixel = transfer(cone.position);
+                    Eigen::Matrix<double,2,1> cone_in_pixel = transfer(cone.position);//调用函数转化
                     
                     if(cone.color.data=="b")
                     {
@@ -96,6 +95,7 @@ int main(int argc,char* argv[])
                     else
                     {cv::circle(img, cv::Point(cone_in_pixel(0,0), cone_in_pixel(1,0)), 5, cv::Scalar(255,0,0), -1);}
                       points.emplace_back(cone_in_pixel[0], cone_in_pixel[1]);
+                      //我在后面补上了颜色识别，用rviz可视化过bag里的Makerarray，看到只有红蓝两种色
                 }
 
 
@@ -113,11 +113,13 @@ int main(int argc,char* argv[])
 
 
     bag.close();
+    //关闭
 
 
-    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(
-    std_msgs::Header(), "bgr8", img).toImageMsg();
-    msg->header.stamp = ros::Time::now();
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg(); 
+    //套用ppt里的模板创建msg
+
+    msg->header.stamp = ros::Time::now();  //时间戳
 
 
     ros::Rate pub_rate(5);  
@@ -126,7 +128,7 @@ int main(int argc,char* argv[])
     pub.publish(msg);
     pub_rate.sleep();
     ros::spinOnce();
-    }
+    } //以设定频率持续发布msg
 
      
     return 0;
